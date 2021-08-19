cf842ad7db0d1216a268536809f71b14b379a582
Saharsh Shukla <67519229+shuklasaharsh@users.noreply.github.com>
2021-08-19 15:08:41 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -2,8 +2,8 @@
 # of time series prediction using a lstm approach. It is provided
 # as is with no warranties or support.
 
-__author__ = "Jakob Aungiers"
-__copyright__ = "Jakob Aungiers 2018"
+__author__ = "Saharsh Shukla"
+__copyright__ = "Saharsh Shukla 2021"
 __version__ = "2.0.0"
 __license__ = "MIT"
 
